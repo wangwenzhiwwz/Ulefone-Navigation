9f12d1e5d08185978ac30186f2210e80f34e975e
WWZ <33089305+wangwenzhiwwz@users.noreply.github.com>
2025-07-09 11:42:52 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+const data = {
+  "ulefone": [
+    {
+      "name": "Google Drive",
+      "url": "https://drive.google.com"
+    },
+    {
+      "name": "Whimsical",
+      "url": "https://whimsical.com"
+    },
+    {
+      "name": "Artlist.io",
+      "url": "https://artlist.io"
+    },
+    {
+      "name": "企业邮箱",
+      "url": "https://work.weixin.qq.com/mail"
+    },
+    {
+      "name": "Notion",
+      "url": "https://www.notion.so/"
+    },
+    {
+      "name": "Mediastory",
+      "url": "https://web.mediastory.cc/#/"
+    },
+    {
+      "name": "企业邮箱",
+      "url": "https://work.weixin.qq.com/mail/"
+    },
+    {
+      "name": "QQ邮箱",
+      "url": "https://mail.qq.com/"
+    }
+  ],
+  "work": [
+    {
+      "name": "产品资料",
+      "url": "https://alidocs.dingtalk.com"
+    },
+    {
+      "name": "测试文案",
+      "url": "https://alidocs.dingtalk.com"
+    }
+  ],
+  "effects": [
+    {
+      "name": "千库网",
+      "url": "https://www.588ku.com"
+    },
+    {
+      "name": "GFXCamp",
+      "url": "https://www.gfxcamp.com"
+    }
+  ]
+};
