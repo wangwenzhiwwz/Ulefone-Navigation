ba448644eaa8da5a3e052c575682937487b01434
WWZ <33089305+wangwenzhiwwz@users.noreply.github.com>
2025-07-10 17:47:44 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const data = {
-  "ulefone": [
+  "Ulefone": [
     {
       "name": "Ulefone",
       "url": "https://www.ulefone.com/"
@@ -41,17 +41,59 @@ const data = {
       "url": "https://www.teambition.com/organization/61ade96d461f91c76e3bfc06/task/shortcut/66d920b8e2a3f00f0ddca2e3/"
     }
   ],
-  "work": [
+  "Work": [
     {
-      "name": "产品资料",
-      "url": "https://alidocs.dingtalk.com"
+      "name": "产品及项目资料",
+      "url": "https://alidocs.dingtalk.com/i/nodes/r1R7q3QmWe74qzKghOqGxy0DJxkXOEP2"
     },
     {
-      "name": "测试文案",
-      "url": "https://alidocs.dingtalk.com"
+      "name": "项目进度管理",
+      "url": "https://alidocs.dingtalk.com/i/nodes/xdqZp24KneBJzgnMX7bL8vyb7RA0Nr1E?utm_scene=team_space&iframeQuery="
+    },
+    {
+      "name": "产品型号及其编码",
+      "url": "https://alidocs.dingtalk.com/i/nodes/Y1OQX0akWm3k9bNOT3q2AlngJGlDd3mE"
+    },
+    {
+      "name": "视频流程注意事项",
+      "url": "https://alidocs.dingtalk.com/i/nodes/20eMKjyp81Rw2aQqczjpbKzrWxAZB1Gv?corpId=dinga746ce2278192d4f35c2f4657eb6378f&cid=47153057863&iframeQuery=utm_medium%3Dim_card%26utm_source%3Dim&utm_scene=team_space"
+    },
+    {
+      "name": "数字资产管理",
+      "url": "https://alidocs.dingtalk.com/i/nodes/pq7N1kjGYznWyOXQZlB3JO43vrPX95oA"
+    },
+    {
+      "name": "器材资产管理",
+      "url": "https://alidocs.dingtalk.com/i/nodes/lo1YvX0prG98kB9db2w9JPw7xzbmLdEZ"
+    },
+    {
+      "name": "专项拍摄方案",
+      "url": "https://alidocs.dingtalk.com/i/nodes/e5vdDPq4wYa8avNrL0ZGWj7nbm10NkB9"
     }
   ],
-  "effects": [
+  "Video": [
+    {
+      "name": "千库网",
+      "url": "https://588ku.com/video/"
+    },
+    {
+      "name": "影视飓风",
+      "url": "https://www.ysjf.com/home"
+    },
+    {
+      "name": "电影色彩",
+      "url": "https://film-grab.com/"
+    },
+    {
+      "name": "Pexels",
+      "url": "https://www.pexels.com/videos/"
+    },
+    {
+      "name": "Pixabay",
+      "url": "https://pixabay.com/zh/videos/"
+    }
+  ],
+  "Effects": [
     {
       "name": "千库网",
       "url": "https://www.588ku.com"
@@ -63,6 +105,10 @@ const data = {
     {
       "name": "GFXCamp",
       "url": "https://www.gfxcamp.com"
+    },
+    {
+      "name": "doyoudo",
+      "url": "https://www.doyoudo.com/"
     }
   ]
 };
