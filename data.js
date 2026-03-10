53982e00620be1c67d20615c8792bc15887e2ed5
WWZ <33089305+wangwenzhiwwz@users.noreply.github.com>
2026-03-10 15:15:16 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -544,7 +544,7 @@ const data = {
         },
         {
             "name": "DE",
-            "url": "https://de.w1997.eu.org:15162/PBYBNXnyr9Jja7lLva/panel/"
+            "url": "https://de.w1997.eu.org:15162/PBYBNXnyr9Jja7lLva/panel/inbounds"
         }
     ],
     "Other": [
